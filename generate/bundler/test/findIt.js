f7e499dc87d7581c3e5360b94ba6e8b22bf92c82
Jordan Nelson <jordan.nelson@sencha.com>
2019-11-14 13:25:48 -0800

    Small test, add findIt tool from marc in test folder

---
+++ generate/bundler/test/findIt.js
@@ -0,0 +1,20 @@
+const fs = require(`fs-extra`);
+const path = require(`path`);
+var str = fs.readFileSync(path.resolve(`./ext.core.prod.js`)).toString()
+// var str = fs.readFileSync(path.resolve(`./ext.grid.prod.js`)).toString()
+var classes = []
+var regex = /Ext.define\('Ext./gi
+var result
+while ( (result = regex.exec(str)) ) {
+    var rawString = str.substring(result.index, result.index+100);
+    var n = rawString.indexOf(`,`);
+    var theClass = str.substring(result.index+12, result.index+n-1);
+    classes.push(theClass)
+}
+classes.sort();
+var countSorted = 0
+classes.forEach(function(classSorted) {
+  countSorted++;
+  var zeroFilledSorted = (`000` + countSorted).substr(-3);
+  console.log(zeroFilledSorted + ` ` + classSorted)
+})
